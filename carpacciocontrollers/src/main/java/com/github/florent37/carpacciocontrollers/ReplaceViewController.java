762af72e606c891275e37524901709a9756fb646
Florent <champigny.florent@gmail.com>
2015-07-25 18:31:21 +0200

    added replaceviewcontroller

---
+++ carpacciocontrollers/src/main/java/com/github/florent37/carpacciocontrollers/ReplaceViewController.java
@@ -0,0 +1,73 @@
+package com.github.florent37.carpacciocontrollers;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.support.v4.util.LruCache;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.lang.reflect.Constructor;
+
+/**
+ * Created by florentchampigny on 21/07/15.
+ */
+public class ReplaceViewController {
+
+    public static final String TAG = "ReplaceViewController";
+
+    public void replace(View view, String viewIdentifier) {
+        Context context = view.getContext();
+        View newView = null;
+        if(viewIdentifier.startsWith("R.layout.") || !viewIdentifier.contains("."))
+            replaceByLayout(context, view,viewIdentifier);
+        else
+            replaceByViewClass(context, view,viewIdentifier);
+
+        if(newView != null) {
+            ViewGroup parent = (ViewGroup) view.getParent();
+
+            newView.setLayoutParams(view.getLayoutParams());
+            newView.setTag(view.getId());
+            newView.setId(view.getId());
+            newView.setBackgroundDrawable(view.getBackground());
+
+            parent.removeView(view);
+            parent.addView(newView);
+        }
+
+        //TODO inject params (text, etc)
+    }
+
+    protected View replaceByViewClass(Context context, View view, String viewClassName) { //com.github.florent37.materialviewpager.MaterialViewPager
+        View newView = null;
+        try{
+            Class viewClass = Class.forName(viewClassName);
+            Constructor<View> constructor = viewClass.getDeclaredConstructor(Context.class);
+            newView = constructor.newInstance(context);
+        }catch (Exception e){
+            Log.e(TAG,"failed to find view class "+viewClassName,e);
+        }
+
+        return newView;
+    }
+
+    protected View replaceByLayout(Context context, View view, String layout) { //R.layout.XXX
+        if(layout.startsWith("R.layout."))
+            layout = layout.replace("R.layout.","");
+
+        int layoutResId = context.getResources().getIdentifier(layout, "layout", context.getPackageName());
+        if(layoutResId == 0) {
+            Log.e(TAG, "failed to find view layout " + layout);
+            return null;
+        }
+        else {
+            ViewGroup parent = (ViewGroup) view.getParent();
+            return LayoutInflater.from(view.getContext()).inflate(layoutResId, parent, false);
+        }
+    }
+
+}
