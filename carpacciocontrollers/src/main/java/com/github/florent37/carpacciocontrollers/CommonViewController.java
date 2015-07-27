537bd203e2bcad77c5acd27d056077e9ba4c554d
Florent <champigny.florent@gmail.com>
2015-07-27 10:15:22 +0200

    added padding() margin()

--- carpacciocontrollers/src/main/java/com/github/florent37/carpacciocontrollers/CommonViewController.java
+++ carpacciocontrollers/src/main/java/com/github/florent37/carpacciocontrollers/CommonViewController.java
@@ -1,9 +1,14 @@
 package com.github.florent37.carpacciocontrollers;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Typeface;
 import android.support.v4.util.LruCache;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 /**
@@ -13,7 +18,33 @@ public class CommonViewController {
 
     private static final String TAG = "CommonViewController";
 
-    public void padding(View view, String paddingLeft, String paddingTop, String paddingRight, String paddingBottom){
+    public static float dpFromPx(final Context context, final float px) {
+        return px / context.getResources().getDisplayMetrics().density;
+    }
 
+    public static float pxFromDp(final Context context, final float dp) {
+        return dp * context.getResources().getDisplayMetrics().density;
+    }
+
+    public void padding(View view, int paddingLeft, int paddingTop, int paddingRight, int paddingBottom){
+        view.setPadding(
+                (int)pxFromDp(view.getContext(),paddingLeft),
+                (int)pxFromDp(view.getContext(),paddingTop),
+                (int)pxFromDp(view.getContext(),paddingRight),
+                (int)pxFromDp(view.getContext(),paddingBottom)
+        );
+    }
+
+    public void margin(View view, int marginLeft, int marginTop, int marginRight, int marginBottom){
+        if(view.getLayoutParams() instanceof ViewGroup.MarginLayoutParams){
+            ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) view.getLayoutParams();
+            lp.setMargins(
+                    (int)pxFromDp(view.getContext(), marginLeft),
+                    (int)pxFromDp(view.getContext(), marginTop),
+                    (int)pxFromDp(view.getContext(), marginRight),
+                    (int)pxFromDp(view.getContext(), marginBottom)
+            );
+            view.setLayoutParams(lp);
+        }
     }
 }
